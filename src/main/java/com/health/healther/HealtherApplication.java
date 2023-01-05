15d8bddb491eaf0b584178bd3a2603d10e9b3e3a
choidongkuen <danaver12@daum.net>
2023-01-05 21:45:20 +0900

    feat : [initial commit] organization 구성 후, 첫 커밋
    특이사항 없음

---
+++ src/main/java/com/health/healther/HealtherApplication.java
@@ -0,0 +1,13 @@
+package com.health.healther;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealtherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealtherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/health/healther/HealtherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.health.healther;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealtherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
