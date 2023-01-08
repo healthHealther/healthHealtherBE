312bf0a11e7a94e07ef45e9fedd5cc970d9945a0
ikjo39 <chi4321@naver.com>
2023-01-08 14:39:25 +0900

    [#1] feat: oauth를 develop을 환경설정과 .gitignore에 application-oauth.yml 추가

--- src/main/java/com/health/healther/HealtherApplication.java
+++ src/main/java/com/health/healther/HealtherApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HealtherApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(HealtherApplication.class, args);
-    }
-
+	public static void main(String[] args) {
+		SpringApplication.run(HealtherApplication.class, args);
+	}
 }
