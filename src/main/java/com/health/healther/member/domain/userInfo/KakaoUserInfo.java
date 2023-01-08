b0055cf5d1dfe925b01a276d4bbabf08a7c891eb
ikjo39 <chi4321@naver.com>
2023-01-09 00:03:51 +0900

    [#1] feat: 고유값과 요청값을 받는 소셜 로그인별 userInfo 생성 - 확장성을 고려하여 interface로 구성

---
+++ src/main/java/com/health/healther/member/domain/userInfo/KakaoUserInfo.java
@@ -0,0 +1,35 @@
+package com.health.healther.member.domain.userInfo;
+
+import java.util.Map;
+
+public class KakaoUserInfo implements OAuth2UserInfo {
+
+	private final Map<String, Object> attributes;
+
+	public KakaoUserInfo(Map<String, Object> attributes) {
+		this.attributes = attributes;
+	}
+
+	@Override
+	public String getProviderId() {
+		return String.valueOf(attributes.get("id"));
+	}
+
+	@Override
+	public String getProvider() {
+		return "kakao";
+	}
+
+	@Override
+	public String getNickName() {
+		return (String) getProfile().get("nickname");
+	}
+
+	private Map<String, Object> getKakaoAccount(){
+		return(Map<String, Object>) attributes.get("kakao_account");
+	}
+
+	private Map<String, Object> getProfile(){
+		return (Map<String, Object>) getKakaoAccount().get("profile");
+	}
+}
---
+++ src/main/java/com/health/healther/member/domain/userInfo/OAuth2UserInfo.java
@@ -0,0 +1,9 @@
+package com.health.healther.member.domain.userInfo;
+
+public interface OAuth2UserInfo {
+	String getProviderId();
+
+	String getProvider();
+
+	String getNickName();
+}
