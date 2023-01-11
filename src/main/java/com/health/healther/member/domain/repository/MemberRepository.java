5ed720bb8f6252b6512bc717d55f366524814a0a
ikjo39 <chi4321@naver.com>
2023-01-11 16:13:20 +0900

    fix: 불필요 어노테이션 제거

--- src/main/java/com/health/healther/member/domain/repository/MemberRepository.java
+++ src/main/java/com/health/healther/member/domain/repository/MemberRepository.java
@@ -3,11 +3,9 @@ package com.health.healther.member.domain.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.health.healther.member.domain.model.Member;
 
-@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 	Optional<Member> findByOauthId(String oauthId);
 }
