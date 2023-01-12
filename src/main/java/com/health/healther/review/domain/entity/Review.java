17c987dbbc5e387a4b97b12ec2ac3a17c79f351b
choidongkuen <danaver12@daum.net>
2023-01-12 14:36:51 +0900

    [#18] feat:ReviewEntity 추가
    Member 엔티티와 Space 엔티티와 연관관계 가집니다.

---
+++ src/main/java/com/health/healther/review/domain/entity/Review.java
@@ -0,0 +1,50 @@
+package com.health.healther.review.domain.entity;
+
+
+import com.health.healther.review.type.Rating;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.*;
+
+@Slf4j
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "review")
+@Entity
+
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "REVIEW_ID", nullable = false)
+    private Long id;
+
+
+    @Column(name = "REVIEW_TITLE", nullable = false) // 제목은 필수
+    private String title;
+
+    @Column(name = "REVIEW_CONTENT")
+    private String content;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "MEMBER_ID")
+//    private Member member;
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "SPACE_ID")
+//    private Space space;
+
+    @Column(name = "GRADE")
+    private Rating rating;
+
+    @Column(name = "IS_DELETED")
+    private boolean isDeleted;
+
+
+}
