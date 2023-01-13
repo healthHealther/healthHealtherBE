9883f3c6ff9d1f5805193e81b9445fa40696a587
choidongkuen <danaver12@daum.net>
2023-01-13 18:12:02 +0900

    [#26] feat : Review NoMemberException 클래스 구현
     사용자 정보가 없는 경우 예외

---
+++ src/main/java/com/health/healther/review/exception/review/NoMemberException.java
@@ -0,0 +1,9 @@
+package com.health.healther.review.exception.review;
+
+public class NoMemberException extends RuntimeException{
+
+    public NoMemberException(String message) {
+
+        super(message);
+    }
+}
