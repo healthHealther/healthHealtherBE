e9600984ae828403608ba9bfb3d9389cb2476a20
choidongkuen <danaver12@daum.net>
2023-01-13 18:11:25 +0900

    [#26] feat : Review NoSpaceException 클래스 구현
    후기에 대한 공간 정보가 없는 경우 예외

---
+++ src/main/java/com/health/healther/review/exception/review/NoSpaceException.java
@@ -0,0 +1,9 @@
+package com.health.healther.review.exception.review;
+
+public class NoSpaceException extends RuntimeException{
+
+    public NoSpaceException(String message) {
+
+        super(message);
+    }
+}
