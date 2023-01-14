8885d1d63f61232fbfeaf8f10cff7c5c70c54b34
ikjo39 <chi4321@naver.com>
2023-01-15 01:23:40 +0900

    [#32] feat: MemberExceptionHandler, MemberCustomException 추가

--- src/main/java/com/health/healther/exception/member/InvalidAccessException.java
+++
@@ -1,7 +0,0 @@
-package com.health.healther.exception.member;
-
-public class InvalidAccessException extends RuntimeException {
-	public InvalidAccessException(String message) {
-		super(message);
-	}
-}
