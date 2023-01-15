5076ff15cd001029530a4df9388672e2ad3776a3
choidongkuen <danaver12@daum.net>
2023-01-15 16:19:40 +0900

    [#26] fix : 후기 등록Controller HTTPSTATUS.CREATED로 변경

--- src/main/java/com/health/healther/review/controller/ReviewController.java
+++ src/main/java/com/health/healther/review/controller/ReviewController.java
@@ -6,10 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -27,6 +24,7 @@ public class ReviewController {
     ) {
 
         reviewService.createReview(request);
-        return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
+
 }
