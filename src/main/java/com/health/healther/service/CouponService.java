d55852607e9cfb940b45937d45cead1e407237a4
HEO <cloudisme99@gmail.com>
2023-01-16 00:45:29 +0900

    [#28] fix: CouponService builder와 return값 리팩토링

--- src/main/java/com/health/healther/service/CouponService.java
+++ src/main/java/com/health/healther/service/CouponService.java
@@ -3,7 +3,6 @@ package com.health.healther.service;
 import static com.health.healther.exception.coupon.CouponErrorCode.*;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.health.healther.domain.model.Coupon;
 import com.health.healther.domain.model.Space;
@@ -22,22 +21,22 @@ public class CouponService {
 
 	private final SpaceRepository spaceRepository;
 
-	@Transactional
-	public Long addCoupon(CouponCreateDto createDto) {
+	public String addCoupon(CouponCreateDto createDto) {
 
-		Space space = spaceRepository.findById(createDto.getSpace().getId())
+		Space space = spaceRepository.findById(createDto.getSpaceId())
 			.orElseThrow(() -> new CouponCustomException(NOT_FOUND_SPACE));
 
-		couponRepository.save(Coupon.builder()
+		Coupon coupon = Coupon.builder()
 			.space(space)
 			.discountAmount(createDto.getDiscountAmount())
 			.openDate(createDto.getOpenDate())
 			.expiredDate(createDto.getExpiredDate())
 			.couponNumber(createDto.getCouponNumber())
 			.amount(createDto.getAmount())
-			.build());
+			.build();
+		couponRepository.save(coupon);
 
-		return createDto.getSpace().getId();
+		return coupon.getSpace().getTitle();
 
 	}
 }
