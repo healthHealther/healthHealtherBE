7a04f33213d50e81bdad962ed30f41eb971cca36
choidongkuen <danaver12@daum.net>
2023-01-15 16:28:07 +0900

    [#42] fix : 후기 삭제 관련 NoFoundReviewException 구현

---
+++ src/main/java/com/health/healther/review/exception/review/NoFoundReviewException.java
@@ -0,0 +1,8 @@
+package com.health.healther.review.exception.review;
+
+public class NoFoundReviewException extends RuntimeException {
+    public NoFoundReviewException(String s) {
+
+        super(s);
+    }
+}
