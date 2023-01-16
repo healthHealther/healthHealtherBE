46063d0575b3046ba345bd2b3d16b32d8d5044a4
choidongkuen <danaver12@daum.net>
2023-01-17 05:48:12 +0900

    [#67] feat : 게시판 조회 컨트롤러 구현

--- src/main/java/com/health/healther/board/controller/BoardController.java
+++ src/main/java/com/health/healther/board/controller/BoardController.java
@@ -1,15 +1,14 @@
 package com.health.healther.board.controller;
 
 import com.health.healther.board.domain.dto.BoardCreateRequestDto;
+import com.health.healther.board.domain.dto.QueryBoardResponseDto;
 import com.health.healther.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import javax.validation.Valid;
 
 @Slf4j
@@ -26,5 +25,15 @@ public class BoardController {
         boardService.createBoard(request);
         return new ResponseEntity<>(HttpStatus.CREATED);
 
+    }
+
+    @GetMapping("/{boardId}")
+    public ResponseEntity<QueryBoardResponseDto> getBoard(
+            @PathVariable("boardId") long boardId ) {
+
+        return new ResponseEntity<>(
+                QueryBoardResponseDto.fromEntity(boardService.getBoard(boardId)),
+                HttpStatus.OK
+        );
     }
 }
