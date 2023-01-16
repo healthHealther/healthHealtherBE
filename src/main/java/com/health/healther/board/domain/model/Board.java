2f8cefaf702b6e402cf5c46247a4e5476f58cad5
choidongkuen <danaver12@daum.net>
2023-01-17 04:32:35 +0900

    [#55] refactor : Board 리팩토링

--- src/main/java/com/health/healther/board/domain/model/Board.java
+++ src/main/java/com/health/healther/board/domain/model/Board.java
@@ -11,18 +11,17 @@ import javax.persistence.ManyToOne;
 
 import com.health.healther.domain.model.BaseEntity;
 import com.health.healther.domain.model.Member;
+import lombok.*;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Where(clause = "DELETED_AT is null")
 @SQLDelete(sql = "UPDATE BOARD SET BOARD.DELETED_AT = CURRENT_TIMESTAMP WHERE BOARD.BOARD_ID = ?")
 @Getter
+@Builder
+@Entity
 public class Board extends BaseEntity {
 
 	@Id
