4b40e9778f43aa11789edc721ce560d90ff41b1b
kdomo <dongho5088@naver.com>
2023-01-16 12:28:37 +0900

    [#35] refactor: 공간 등록 API 리팩토링

--- src/main/java/com/health/healther/domain/model/Space.java
+++ src/main/java/com/health/healther/domain/model/Space.java
@@ -12,14 +12,16 @@ import javax.persistence.ManyToOne;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
-import com.health.healther.dto.space.CreateSpaceRequestDto;
-
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 @Where(clause = "DELETED_AT is null")
 @SQLDelete(sql = "UPDATE SPACE SET SAPCE.DELETED_AT = CURRENT_TIMESTAMP WHERE SPACE.SAPCE_ID = ?")
 @Getter
@@ -54,39 +56,4 @@ public class Space extends BaseEntity {
 
 	@Column(name = "PRICE")
 	private int price;
-
-	private Space(
-			Member member,
-			String title,
-			String content,
-			String address,
-			String addressDetail,
-			String notice,
-			String rule,
-			int price
-	) {
-		this.member = member;
-		this.title = title;
-		this.content = content;
-		this.address = address;
-		this.addressDetail = addressDetail;
-		this.notice = notice;
-		this.rule = rule;
-		this.price = price;
-	}
-
-	public static Space of(
-			CreateSpaceRequestDto createSpaceRequestDto
-	) {
-		return new Space(
-				null,
-				createSpaceRequestDto.getTitle(),
-				createSpaceRequestDto.getContent(),
-				createSpaceRequestDto.getAddress(),
-				createSpaceRequestDto.getAddressDetail(),
-				createSpaceRequestDto.getNotice(),
-				createSpaceRequestDto.getRule(),
-				createSpaceRequestDto.getPrice()
-		);
-	}
 }
