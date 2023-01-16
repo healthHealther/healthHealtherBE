99316616a670ebe46d10e7f4146bf44ddcc97a4a
choidongkuen <danaver12@daum.net>
2023-01-17 04:21:23 +0900

    [#55] feat : BoardController 구현

--- src/main/java/com/health/healther/board/controller/BoardController.java
+++ src/main/java/com/health/healther/board/controller/BoardController.java
@@ -1,4 +1,31 @@
 package com.health.healther.board.controller;
 
+import com.health.healther.board.domain.dto.BoardCreateRequestDto;
+import com.health.healther.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/board")
+@RestController
 public class BoardController {
+
+    private final BoardService boardService;
+    @PostMapping
+    public ResponseEntity<Object> createBoard(
+            @RequestBody @Valid BoardCreateRequestDto request) {
+
+        boardService.createBoard(request);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+
+    }
 }
