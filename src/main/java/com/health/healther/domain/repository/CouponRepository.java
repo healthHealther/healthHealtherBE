267ee908b313c148bc0ffe8f3228f84e5c04ad9d
HEO <cloudisme99@gmail.com>
2023-01-17 04:11:59 +0900

    [#63] feat: 쿠폰 조회 API

--- src/main/java/com/health/healther/domain/repository/CouponRepository.java
+++ src/main/java/com/health/healther/domain/repository/CouponRepository.java
@@ -1,8 +1,13 @@
 package com.health.healther.domain.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.health.healther.domain.model.Coupon;
 
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
+
+	Optional<Coupon> findBySpace_Id(Long spaceId);
+
 }
