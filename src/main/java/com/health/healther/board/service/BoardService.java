d33f96c1e0ad4cea499c8f1f8dbd5a0d53b2596d
choidongkuen <danaver12@daum.net>
2023-01-17 04:31:45 +0900

    [#55] feat : BoardService 구현

--- src/main/java/com/health/healther/board/service/BoardService.java
+++ src/main/java/com/health/healther/board/service/BoardService.java
@@ -1,4 +1,7 @@
 package com.health.healther.board.service;
 
+import com.health.healther.board.domain.dto.BoardCreateRequestDto;
+
 public interface BoardService {
+    void createBoard(BoardCreateRequestDto request);
 }
--- src/main/java/com/health/healther/board/service/impl/BoardServiceImpl.java
+++ src/main/java/com/health/healther/board/service/impl/BoardServiceImpl.java
@@ -1,6 +1,34 @@
 package com.health.healther.board.service.impl;
 
+import com.health.healther.board.domain.dto.BoardCreateRequestDto;
+import com.health.healther.board.domain.model.Board;
+import com.health.healther.board.domain.repository.BoardRepository;
 import com.health.healther.board.service.BoardService;
+import com.health.healther.domain.model.Member;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
 public class BoardServiceImpl implements BoardService {
+
+    private final BoardRepository boardRepository;
+
+    @Transactional
+    @Override
+    public void createBoard(BoardCreateRequestDto request) {
+
+        Member member = memberService.findUserFromToken();
+
+        boardRepository.save(Board.builder()
+                              .member(member)
+                              .title(request.getTitle())
+                              .content(request.getContent())
+                              .build()
+        );
+    }
 }
