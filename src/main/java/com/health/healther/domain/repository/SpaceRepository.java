f9a48ff6964a466771253d995ee0451531c0e162
kdomo <dongho5088@naver.com>
2023-01-19 13:50:22 +0900

    [#70] fix: 주석 제거

--- src/main/java/com/health/healther/domain/repository/SpaceRepository.java
+++ src/main/java/com/health/healther/domain/repository/SpaceRepository.java
@@ -15,13 +15,4 @@ public interface SpaceRepository extends JpaRepository<Space, Long> {
 			+ " join fetch s.images"
 			+ " where s.id = :spaceId")
 	Optional<Space> findByIdUseFetchJoin(Long spaceId);
-
-	// @Query("select s from Space s join fetch s.spaceTime join fetch s.spaceKinds join fetch s.conveniences join fetch s.images "
-	// 		+ "where s.title like %:title%")
-	// @EntityGraph(attributePaths = {"spaceTime", "spaceKinds", "conveniences", "images"})
-	// @Query("select s from Space s left join s.spaceTime left join s.spaceKinds left join s.conveniences left join s.images "
-	// 		+ "where s.title like %:title%")
-	// List<Space> findAllByIdUseFetchJoin(String title, Pageable pageable);
-
-	// Page<Space> findAllByTitleContainingIgnoreCase(String title);
 }
