8a7acbd32b792d7712aef366ae261c6a14c7360f
choidongkuen <danaver12@daum.net>
2023-01-25 18:14:28 +0900

    [#100] feat : 게시판 상세조회 서비스 구현

--- src/main/java/com/health/healther/service/BoardService.java
+++ src/main/java/com/health/healther/service/BoardService.java
@@ -4,7 +4,10 @@ import com.health.healther.domain.model.Board;
 import com.health.healther.domain.model.Member;
 import com.health.healther.domain.repository.BoardRepository;
 import com.health.healther.dto.board.BoardCreateRequestDto;
+import com.health.healther.dto.board.BoardDetailResponseDto;
+import com.health.healther.exception.board.NotFoundBoardException;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Not;
 import org.springframework.stereotype.Service;
 
 
@@ -26,5 +29,13 @@ public class BoardService {
                                   .content(request.getContent())
                                   .likeCount(0)
                                   .build());
+    }
+
+    public BoardDetailResponseDto getBoardDetail(Long id) {
+
+        Board board = boardRepository.findById(id)
+                .orElseThrow(() -> new NotFoundBoardException("게시판 정보를 찾을 수 없습니다."));
+
+        return BoardDetailResponseDto.of(board);
     }
 }
