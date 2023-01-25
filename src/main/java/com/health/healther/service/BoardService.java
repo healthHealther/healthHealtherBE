c7fd5b49277f2fa3242ed057d3b904cdfc0fc7f5
choidongkuen <danaver12@daum.net>
2023-01-25 17:35:06 +0900

    [#98] feat : 게시판 등록 서비스 구현

---
+++ src/main/java/com/health/healther/service/BoardService.java
@@ -0,0 +1,14 @@
+package com.health.healther.service;
+
+import com.health.healther.dto.board.BoardCreateRequestDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class BoardService {
+    public void createBoard(BoardCreateRequestDto request) {
+    }
+}
