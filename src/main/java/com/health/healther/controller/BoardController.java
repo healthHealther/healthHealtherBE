34800e0c18bff77348595a199f45a5ceb7562efa
choidongkuen <danaver12@daum.net>
2023-01-25 13:51:04 +0900

    [#91] feat : 게시판 등록 컨트롤러 구현

---
+++ src/main/java/com/health/healther/controller/BoardController.java
@@ -0,0 +1,30 @@
+package com.health.healther.controller;
+
+
+import com.health.healther.dto.board.BoardCreateRequestDto;
+import com.health.healther.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@Slf4j
+@RequestMapping("/board")
+@RequiredArgsConstructor
+@RestController
+public class BoardController {
+
+    private final BoardService boardService;
+
+    @PostMapping
+    public ResponseEntity createBoard(
+            @RequestBody @Valid BoardCreateRequestDto request) {
+        return new ResponseEntity<>(boardService.createBoard(request), HttpStatus.CREATED);
+    }
+}
