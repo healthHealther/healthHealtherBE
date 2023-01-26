825b5053730785379977d284db040d14f2fd1a7b
choidongkuen <danaver12@daum.net>
2023-01-27 06:51:19 +0900

    [#119] feat : 게시판 추천 취소 서비스 구현

--- src/main/java/com/health/healther/service/BoardService.java
+++ src/main/java/com/health/healther/service/BoardService.java
@@ -1,6 +1,7 @@
 package com.health.healther.service;
 
 import com.health.healther.domain.model.Board;
+import com.health.healther.domain.model.BoardLike;
 import com.health.healther.domain.model.Member;
 import com.health.healther.domain.repository.BoardLikeRepository;
 import com.health.healther.domain.repository.BoardRepository;
@@ -8,6 +9,7 @@ import com.health.healther.domain.repository.CommentRepository;
 import com.health.healther.dto.board.BoardCreateRequestDto;
 import com.health.healther.dto.board.BoardDetailResponseDto;
 import com.health.healther.exception.board.NotFoundBoardException;
+import com.health.healther.exception.board.NotFoundBoardLikeException;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.weaver.ast.Not;
 import org.springframework.stereotype.Service;
@@ -46,5 +48,21 @@ public class BoardService {
                 .orElseThrow(() -> new NotFoundBoardException("게시판 정보를 찾을 수 없습니다."));
 
         return BoardDetailResponseDto.of(board);
+    }
+
+    @Transactional
+    public void deleteBoardLike(Long id) {
+
+        Member member = memberService.findUserFromToken();
+
+        Board board = boardRepository.findById(id)
+                .orElseThrow(() -> new NotFoundBoardException("게시판 정보를 찾을 수 없습니다."));
+
+        BoardLike boardLike = boardLikeRepository.findByMemberAndBoard(member,board)
+                 .orElseThrow(() -> new NotFoundBoardLikeException("추천 정보를 찾을 수 없습니다."));
+
+        boardLikeRepository.delete(boardLike);
+
+        board.deleteBoardLike();
     }
 }
