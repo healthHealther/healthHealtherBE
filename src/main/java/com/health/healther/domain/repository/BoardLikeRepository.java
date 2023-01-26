2b95b1b4878a76bfdf9a9f75da60d71bfd3bcedb
choidongkuen <danaver12@daum.net>
2023-01-27 07:10:43 +0900

    [#117] feat : findByMemberAndBoard 메소드 추가

--- src/main/java/com/health/healther/domain/repository/BoardLikeRepository.java
+++ src/main/java/com/health/healther/domain/repository/BoardLikeRepository.java
@@ -1,8 +1,13 @@
 package com.health.healther.domain.repository;
 
+import com.health.healther.domain.model.Board;
+import com.health.healther.domain.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.health.healther.domain.model.BoardLike;
 
+import java.util.Optional;
+
 public interface BoardLikeRepository extends JpaRepository<BoardLike, Long> {
+    Optional<BoardLike> findByMemberAndBoard(Member member, Board board);
 }
