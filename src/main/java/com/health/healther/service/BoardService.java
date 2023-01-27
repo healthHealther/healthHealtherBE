6e9d4f16f33840210618baf36f109cd48ee3c51c
choidongkuen <danaver12@daum.net>
2023-01-27 16:18:56 +0900

    [#123] feat : 게시글 등록 서비스 리팩토링
    동일한 회원이 같은 제목의 게시글 게시를 할 때 오류[SameBoardAlreadyExistException] 발생

--- src/main/java/com/health/healther/service/BoardService.java
+++ src/main/java/com/health/healther/service/BoardService.java
@@ -8,6 +8,7 @@ import com.health.healther.domain.repository.CommentRepository;
 import com.health.healther.dto.board.BoardCreateRequestDto;
 import com.health.healther.dto.board.BoardDetailResponseDto;
 import com.health.healther.exception.board.NotFoundBoardException;
+import com.health.healther.exception.board.SameBoardAlreadyExistException;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.weaver.ast.Not;
 import org.springframework.stereotype.Service;
@@ -30,6 +31,10 @@ public class BoardService {
     public void createBoard(BoardCreateRequestDto request) {
 
         Member member = memberService.findUserFromToken();
+
+        if(boardRepository.findByTitleAndMember(request.getTitle(), member).isPresent()) {
+            throw new SameBoardAlreadyExistException("동일한 제목의 게시글이 이미 존재합니다.");
+        }
 
         boardRepository.save(Board.builder()
                                   .member(member)
