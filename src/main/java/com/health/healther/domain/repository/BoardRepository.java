d6949757a2ecb9fbef90583d28ec675d04562e8d
choidongkuen <danaver12@daum.net>
2023-01-27 16:18:03 +0900

    [#123] feat : BoardRepository 메소드 추가

--- src/main/java/com/health/healther/domain/repository/BoardRepository.java
+++ src/main/java/com/health/healther/domain/repository/BoardRepository.java
@@ -1,8 +1,12 @@
 package com.health.healther.domain.repository;
 
+import com.health.healther.domain.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.health.healther.domain.model.Board;
 
+import java.util.Optional;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    Optional<Board> findByTitleAndMember(String title , Member member);
 }
