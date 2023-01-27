addedf89a82d03780a7796ad1ddd0ecd86f63366
choidongkuen <danaver12@daum.net>
2023-01-27 12:54:34 +0900

    [#121] feat : 게시판 본인 추천 여부 조회 서비스 구현

--- src/main/java/com/health/healther/service/BoardService.java
+++ src/main/java/com/health/healther/service/BoardService.java
@@ -7,6 +7,7 @@ import com.health.healther.domain.repository.BoardRepository;
 import com.health.healther.domain.repository.CommentRepository;
 import com.health.healther.dto.board.BoardCreateRequestDto;
 import com.health.healther.dto.board.BoardDetailResponseDto;
+import com.health.healther.dto.board.BoardIsLikedResponseDto;
 import com.health.healther.exception.board.NotFoundBoardException;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.weaver.ast.Not;
@@ -46,5 +47,19 @@ public class BoardService {
                 .orElseThrow(() -> new NotFoundBoardException("게시판 정보를 찾을 수 없습니다."));
 
         return BoardDetailResponseDto.of(board);
+    }
+
+    @Transactional(readOnly = true)
+    public BoardIsLikedResponseDto boardIsLiked(Long id) {
+
+        Member member = memberService.findUserFromToken();
+
+        Board board = boardRepository.findById(id)
+                .orElseThrow(() -> new NotFoundBoardException("게시판 정보를 찾을 수 없습니다."));
+
+        if(boardLikeRepository.findByMemberAndBoard(member,board).isEmpty()) {
+            return BoardIsLikedResponseDto.of(false);
+        }
+        return BoardIsLikedResponseDto.of(true);
     }
 }
