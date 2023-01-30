0d3af656158b9b71396776d7ce9484d050c5e749
choidongkuen <danaver12@daum.net>
2023-01-30 19:50:00 +0900

    [#160] refactor : Review 라인 조정 및 기타 리팩토링

--- src/main/java/com/health/healther/domain/model/Review.java
+++ src/main/java/com/health/healther/domain/model/Review.java
@@ -1,13 +1,10 @@
 package com.health.healther.domain.model;
 
-import javax.persistence.*;
-
-import com.health.healther.domain.model.BaseEntity;
-import com.health.healther.domain.model.Member;
-import com.health.healther.domain.model.Space;
 import lombok.*;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
@@ -18,35 +15,33 @@ import org.hibernate.annotations.Where;
 @Entity
 public class Review extends BaseEntity {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "REVIEW_ID")
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "REVIEW_ID")
+    private Long id;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "MEMBER_ID")
-	private Member member;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "SPACE_ID")
-	private Space space;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "SPACE_ID")
+    private Space space;
 
-	@Column(name = "TITLE")
-	private String title;
+    @Column(name = "TITLE")
+    private String title;
 
-	@Lob
-	@Column(name = "CONTENT")
-	private String content;
+    @Lob
+    @Column(name = "CONTENT")
+    private String content;
 
-	@Column(name = "GRADE")
-	private int grade;
+    @Column(name = "GRADE")
+    private int grade;
 
-	public void updateReview(String title, String content, int grade) {
+    public void updateReview(String title, String content, int grade) {
 
-		this.title = title;
-		this.content = content;
-		this.grade = grade;
-	}
-
-
+        this.title = title;
+        this.content = content;
+        this.grade = grade;
+    }
 }
