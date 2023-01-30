e9cce747741c2c453867c65da12f5a717a2a031b
kdomo <dongho5088@naver.com>
2023-01-30 16:36:34 +0900

    [#156] fix: 공간 타입 변경

--- src/main/java/com/health/healther/constant/SpaceType.java
+++ src/main/java/com/health/healther/constant/SpaceType.java
@@ -8,7 +8,8 @@ import lombok.Getter;
 public enum SpaceType {
 	GX("GX"),
 	PILATES("PILATES"),
-	YOGA("YOGA");
+	ANAEROBIC("ANAEROBIC"),
+	AEROBIC("AEROBIC");
 
 	@Getter
 	private final String value;
