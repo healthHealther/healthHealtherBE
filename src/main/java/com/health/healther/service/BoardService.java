51b094088dd61f51a29ad41cef180cc1ac11265d
choidongkuen <danaver12@daum.net>
2023-01-30 15:09:20 +0900

    [#152] feat : 게시판 조회 서비스 구현

--- src/main/java/com/health/healther/service/BoardService.java
+++ src/main/java/com/health/healther/service/BoardService.java
@@ -9,13 +9,15 @@ import com.health.healther.domain.repository.BoardRepository;
 import com.health.healther.domain.repository.CommentRepository;
 import com.health.healther.dto.board.*;
 import com.health.healther.exception.board.BoardLikeAlreadyExistException;
-import com.health.healther.dto.board.BoardCreateRequestDto;
-import com.health.healther.dto.board.BoardDetailResponseDto;
 import com.health.healther.exception.board.NotFoundBoardException;
 import com.health.healther.exception.board.NotFoundBoardLikeException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -71,7 +73,7 @@ public class BoardService {
     public void updateBoard(Long id, BoardUpdateRequestDto request) {
 
         Board board = boardRepository.findById(id)
-                .orElseThrow(() -> new NotFoundBoardException("게시판 정보를 찾을 수 없습니다."));
+                                     .orElseThrow(() -> new NotFoundBoardException("게시판 정보를 찾을 수 없습니다."));
 
         board.updateBoard(request);
 
@@ -139,6 +141,14 @@ public class BoardService {
                                       .build());
 
         return new CommentRegisterResponseDto(request.getContext());
+    }
+
+    public List<GetBoardListResponseDto> getBoardList(GetBoardListRequestDto request) {
+
+        PageRequest pageRequest = PageRequest.of(request.getPage(), request.getSize());
+        return boardRepository.findAll(pageRequest).stream()
+                .map(GetBoardListResponseDto :: from)
+                .collect(Collectors.toList());
     }
 }
 
