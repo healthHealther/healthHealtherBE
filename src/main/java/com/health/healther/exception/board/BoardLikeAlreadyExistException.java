f17163d020fc821aae2deb5430e85cb89b2dcfc2
choidongkuen <danaver12@daum.net>
2023-01-31 00:58:24 +0900

    [#165] refactor : AlreadyBoardLikeException 리팩토링

--- src/main/java/com/health/healther/exception/board/BoardLikeAlreadyExistException.java
+++
@@ -1,7 +0,0 @@
-package com.health.healther.exception.board;
-
-public class BoardLikeAlreadyExistException extends RuntimeException{
-    public BoardLikeAlreadyExistException(String message) {
-        super(message);
-    }
-}
