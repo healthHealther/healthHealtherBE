cca83982780215e3c866433a269da5970782b1cb
choidongkuen <danaver12@daum.net>
2023-01-30 14:10:29 +0900

    [#135] feat : 게시판 댓글 조회 service 뼈대 구현

--- src/main/java/com/health/healther/service/BoardService.java
+++ src/main/java/com/health/healther/service/BoardService.java
@@ -17,6 +17,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 
 @RequiredArgsConstructor
 @Service
@@ -139,6 +141,10 @@ public class BoardService {
                                       .build());
 
         return new CommentRegisterResponseDto(request.getContext());
+    }
+
+    public List<CommentListResponseDto> getCommentList(Long id, CommentListRequestDto request) {
+        return null;
     }
 }
 
