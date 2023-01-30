8eb839a89d02e30431fadb63153fa601c2032e95
choidongkuen <danaver12@daum.net>
2023-01-30 16:55:57 +0900

    [#135] feat : CommentRepository 페이징 관련 메소드 추가

--- src/main/java/com/health/healther/domain/repository/CommentRepository.java
+++ src/main/java/com/health/healther/domain/repository/CommentRepository.java
@@ -1,8 +1,12 @@
 package com.health.healther.domain.repository;
 
+import com.health.healther.domain.model.Board;
+import com.health.healther.domain.model.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.health.healther.domain.model.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    Page<Comment> findByBoard(Board board, PageRequest pageRequest);
 }
