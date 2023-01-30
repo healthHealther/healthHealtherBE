8b8975614e27b2c1dc0cda468c4557b4f20f5960
choidongkuen <danaver12@daum.net>
2023-01-30 18:22:40 +0900

    [#160] refactor : 후기 등록 관련 서비스 리팩토링

--- src/main/java/com/health/healther/service/ReviewService.java
+++ src/main/java/com/health/healther/service/ReviewService.java
@@ -2,18 +2,16 @@ package com.health.healther.service;
 
 
 import com.health.healther.domain.model.Member;
+import com.health.healther.domain.model.Review;
 import com.health.healther.domain.model.Space;
 import com.health.healther.domain.repository.MemberRepository;
+import com.health.healther.domain.repository.ReviewRepository;
 import com.health.healther.domain.repository.SpaceRepository;
 import com.health.healther.dto.review.ReviewCreateRequestDto;
 import com.health.healther.dto.review.ReviewDto;
 import com.health.healther.dto.review.ReviewRequestUpdateDto;
-import com.health.healther.domain.model.Review;
-import com.health.healther.domain.repository.ReviewRepository;
-import com.health.healther.exception.review.NotFoundMemberException;
 import com.health.healther.exception.review.NotFoundReviewException;
 import com.health.healther.exception.space.NotFoundSpaceException;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -31,6 +29,7 @@ public class ReviewService {
 
     private final SpaceRepository spaceRepository;
 
+
     private final MemberRepository memberRepository;
 
     private final MemberService memberService;
@@ -41,22 +40,22 @@ public class ReviewService {
         Member member = memberService.findUserFromToken();
 
         Space space = spaceRepository.findById(request.getSpaceId())
-                .orElseThrow(() -> new NotFoundSpaceException("공간 정보를 찾을 수 없습니다."));
+                                     .orElseThrow(() -> new NotFoundSpaceException("공간 정보를 찾을 수 없습니다."));
 
         reviewRepository.save(
                 Review.builder()
-                        .member(member)
-                        .space(space)
-                        .title(request.getTitle())
-                        .content(request.getContent())
-                        .grade(request.getGrade())
-                        .build());
+                      .member(member)
+                      .space(space)
+                      .title(request.getTitle())
+                      .content(request.getContent())
+                      .grade(request.getGrade())
+                      .build());
     }
 
     @Transactional
     public void deleteReview(Long reviewId) {
         Review review = reviewRepository.findById(reviewId)
-                    .orElseThrow(() -> new NotFoundReviewException("일치하는 후기 정보가 존재하지 않습니다."));
+                                        .orElseThrow(() -> new NotFoundReviewException("일치하는 후기 정보가 존재하지 않습니다."));
 
         reviewRepository.delete(review);
     }
