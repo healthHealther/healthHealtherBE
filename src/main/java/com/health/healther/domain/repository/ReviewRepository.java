841cb9ec3180740abe2abec9f9060147a0d27df4
choidongkuen <danaver12@daum.net>
2023-01-31 20:37:04 +0900

    [#160] feat : ReviewRepository 메소드 추가

--- src/main/java/com/health/healther/domain/repository/ReviewRepository.java
+++ src/main/java/com/health/healther/domain/repository/ReviewRepository.java
@@ -1,8 +1,13 @@
 package com.health.healther.domain.repository;
 
+import com.health.healther.domain.model.Member;
+import com.health.healther.domain.model.Space;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.health.healther.domain.model.Review;
 
+import java.util.Optional;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    Optional<Review> findByMemberAndSpace(Member member, Space space);
 }
