b02cd280ffd78b2a9346fb6ca1e061bda8f868a7
choidongkuen <danaver12@daum.net>
2023-01-31 20:36:04 +0900

    [#160] feat : AlreadyCreateReviewException 구현

---
+++ src/main/java/com/health/healther/exception/review/AlreadyCreateReviewException.java
@@ -0,0 +1,7 @@
+package com.health.healther.exception.review;
+
+public class AlreadyCreateReviewException extends RuntimeException {
+    public AlreadyCreateReviewException(String message) {
+        super(message);
+    }
+}
