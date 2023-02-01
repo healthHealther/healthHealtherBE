08fafadbbcbb6cda66dd5fa0e165624e1de916f4
ikjo39 <chi4321@naver.com>
2023-02-01 13:08:51 +0900

    test: CORS-구체적인 originPattern 추가

--- src/main/java/com/health/healther/config/WebMvcConfig.java
+++ src/main/java/com/health/healther/config/WebMvcConfig.java
@@ -1,20 +1,26 @@
 package com.health.healther.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import lombok.RequiredArgsConstructor;
-
 @Configuration
-@RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
+	private final String originPattern;
+
+	public WebMvcConfig(
+		@Value("${cors.origin}") String originPattern
+	) {
+		this.originPattern = originPattern;
+	}
+
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 			.allowedHeaders("*")
-			.allowedOriginPatterns("*")
+			.allowedOriginPatterns(originPattern)
 			.exposedHeaders("*")
 			.allowCredentials(true);
 		WebMvcConfigurer.super.addCorsMappings(registry);
